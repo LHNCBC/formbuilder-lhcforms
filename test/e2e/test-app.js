b259377aadf23e82f2059cfbc18f45158340c838
Kanduru, Ajay (NIH/NLM/LHC) [C] <akanduru@mail.nih.gov>
2019-04-11 12:38:48 -0400

    Remove forced templateOptions in the export of lforms format and fix restriction data struction to conform to lforms spec

--- test/e2e/test-app.js
+++ test/e2e/test-app.js
@@ -3,4 +3,5 @@ const fhirMock = require('./formbuilder/fhir-mock');
 
 fhirMock('http://hapi.fhir.org').persist();
 let app = require('../../server').createFormbuilder(require('../../formbuilder.conf'));
+console.log('Starting test server...');
 app.start();
