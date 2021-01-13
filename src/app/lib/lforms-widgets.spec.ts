5b96e3898b69e0dd52a658b0d05d6c86524b985d
akanduru <akanduru@mail.nih.gov>
2021-01-13 16:49:59 -0500

    Reset code base for new formbuilder

--- src/app/lib/lforms-widgets.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LformsWidgetRegistry } from './lforms-widget-registry';
-
-describe('LformsWidgets', () => {
-  it('should create an instance', () => {
-    expect(new LformsWidgetRegistry()).toBeTruthy();
-  });
-});
--- src/app/lib/pipes/app-json.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppJsonPipe } from './app-json.pipe';
-
-describe('AppJsonPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AppJsonPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
