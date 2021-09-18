db466ffc5dc0577940a611ce7c2841e776a3b1f4
akanduru <akanduru@mail.nih.gov>
2021-09-18 14:36:45 -0400

    Split cypress spec files

---
+++ cypress/integration/item-level/item-level.spec.js
@@ -0,0 +1,75 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Home page', () => {
+  before(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/')
+    cy.get('button').contains('Continue').click();
+  })
+
+  beforeEach(() => {
+    cy.get('#Yes_1').find(':radio').as('codeYes');
+    cy.get('#No_1').find(':radio').as('codeNo');
+  });
+
+  context('Item level fields', () => {
+    before(() => {
+      cy.get('button').contains('Create questions').click();
+    });
+
+    it('should display item editor page', () => {
+      cy.get('tree-root tree-viewport tree-node-collection tree-node').first().should('be.visible');
+      cy.get('@codeYes').check({force: true});
+      cy.get('#code\\.0\\.code').as('code');
+      cy.get('@code').should('be.visible');
+      cy.get('@codeNo').check({force: true});
+      cy.get('@code').should('not.exist');
+    });
+
+    it('should add a new item', () => {
+      cy.contains('Add new item').scrollIntoView().click();
+      cy.get('tree-root tree-viewport tree-node-collection tree-node span').last().should('have.text', 'New item 1');
+      cy.contains('Delete this item').scrollIntoView().click();
+      cy.get('tree-root tree-viewport tree-node-collection tree-node span').last().should('have.text', 'Item 0');
+    });
+
+    it('should display quantity units', () => {
+      cy.get('#type').select('string');
+      cy.get('[id^="units"]').should('not.exist');
+      cy.get('#type').select('quantity');
+      cy.get('[id^="units"]').last().as('units');
+      cy.get('@units').should('be.visible');
+      cy.get('#searchResults').should('not.be.visible');
+      cy.get('@units').type('inch');
+      cy.contains('#completionOptions tr', '[in_i]').click();
+      cy.contains('span.autocomp_selected li', '[in_i]').should('be.visible');
+      cy.contains('#completionOptions tr', '[in_br]').click();
+      cy.contains('span.autocomp_selected li', '[in_br]').should('be.visible');
+    });
+    it('should display decimal/integer units', () => {
+      cy.get('#type').select('string');
+      cy.get('[id^="units"]').should('not.exist');
+      cy.get('#type').select('decimal');
+      cy.get('[id^="units"]').last().as('units');
+      cy.get('@units').should('be.visible');
+      cy.get('#searchResults').should('not.be.visible');
+      cy.get('@units').type('inch');
+      cy.contains('#completionOptions tr', '[in_i]').click();
+      cy.get('@units').last().should('have.value','[in_i]');
+    });
+  });
+})
