b9a5998afea89acfbc43df7ee859b4c6f674f70a
Kanduru, Ajay (NIH/NLM/LHC) [C] <akanduru@mail.nih.gov>
2022-01-04 21:30:52 -0500

    Remove old files

--- client/app/firebase/firebase-client-authorization.js.tpl
+++
@@ -1,8 +0,0 @@
-var firebaseConfig = {
-  apiKey: "<%= apiKey %>",
-  authDomain: "<%= authDomain %>",
-  databaseURL: "<%= databaseURL %>",
-  projectId: "<%= projectId %>",
-  storageBucket: "<%= storageBucket %>",
-  messagingSenderId: "<%= messagingSenderId %>"
-};
--- test/e2e/test-app.js
+++
@@ -1,8 +0,0 @@
-
-const fhirMock = require('./formbuilder/fhir-mock');
-
-fhirMock('http://hapi.fhir.org').persist();
-console.log('Starting test-app.js ...');
-let app = require('../../server').createFormbuilder(require('../../formbuilder.conf'));
-console.log('Starting test server...');
-app.start();
