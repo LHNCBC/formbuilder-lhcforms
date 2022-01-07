215b5f4df6f6e84f187104e09a63e0fcf8cebbc8
Kanduru, Ajay (NIH/NLM/LHC) [C] <akanduru@mail.nih.gov>
2022-01-07 11:01:30 -0500

    Address review comments

--- bin/copy-lforms.js
+++ bin/copy-lforms.js
@@ -1,5 +1,5 @@
 /**
- * This script copies lforms build files from CTSS website.
+ * This script copies lforms build files from LForms website.
  */
 
 const https = require('https');
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 
 const destinationFolder = path.join(__dirname, '../src/lib/lforms/lib');
-const ctssUrl = 'https://clinicaltables.nlm.nih.gov/lforms-versions';
+const downloadUrl = 'https://clinicaltables.nlm.nih.gov/lforms-versions';
 const version = '30.0.0-beta.1';
 const filePathMap = {
   'webcomponent/lhc-forms.es5.js'   : 'elements',
@@ -25,31 +25,14 @@ if(!fs.existsSync(appFolder)) {
   errorExit(null, `${appFolder} directory does not exist. Please make sure to create it before running this script`);
 }
 
-
-const fileMapArray = getUrlToFilePath(filePathMap);
-
-fileMapArray.forEach((m) => {
+Object.keys(filePathMap).map((k) => {
+ // Create a map between url to destination file path.
+  let url = [downloadUrl, version, k].join('/');
+  let file = [destinationFolder, filePathMap[k], path.basename(k)].join('/');
+  return {url, file};
+}).forEach((m) => {
   copyFileFromUrl(m.url, m.file);
 });
-
-
-/**
- * Create a map between url to destination file path.
- *
- * @param aFilePathMap - File path map defined above.
- * @returns {*[]}
- */
-function getUrlToFilePath(aFilePathMap) {
-  const ret = [];
-  Object.keys(aFilePathMap).forEach((k) => {
-    const map = {};
-    let url = [ctssUrl, version, k].join('/');
-    let file = [destinationFolder, filePathMap[k], path.basename(k)].join('/');
-    ret.push({url, file});
-  });
-
-  return ret;
-}
 
 
 /**
@@ -78,7 +61,8 @@ function copyFileFromUrl(url, filePath) {
   file.on('error', (e) => {
     errorExit(e, `Error writing file ${filePath}`);
   });
-  const request = https.get(url, function(response) {
+
+  https.get(url, function(response) {
     if(response.statusCode === 200) {
       response.pipe(file);
     }
