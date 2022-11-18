72535adca22b07d65e457dac5d2e1ac5fa98b412
Kanduru, Ajay (NIH/NLM/LHC) [C] <akanduru@mail.nih.gov>
2022-11-18 18:42:18 -0500

    Fix attr.type in NumberComponent and simplify the code.

--- src/app/lib/widgets/number/number.component.ts
+++ src/app/lib/widgets/number/number.component.ts
@@ -1,6 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
-import {FormProperty} from '@lhncbc/ngx-schema-form';
-
+import { Component } from '@angular/core';
 import {LfbControlWidgetComponent} from '../lfb-control-widget/lfb-control-widget.component';
 
 /**
@@ -22,7 +20,7 @@ import {LfbControlWidgetComponent} from '../lfb-control-widget/lfb-control-widge
         <input [attr.readonly]="schema.readOnly?true:null" [attr.name]="name"
                [attr.id]="id"
                class="text-widget integer-widget form-control {{controlWidthClass}}" [formControl]="control"
-               type="text"
+               type="number" [attr.min]="schema.minimum" [attr.max]="schema.maximum"
                [attr.placeholder]="schema.placeholder"
                [attr.maxLength]="schema.maxLength || null"
                [attr.minLength]="schema.minLength || null">
@@ -31,64 +29,5 @@ import {LfbControlWidgetComponent} from '../lfb-control-widget/lfb-control-widge
 
   `
 })
-export class NumberComponent extends LfbControlWidgetComponent implements AfterViewInit {
-  controlValue: string;
-
-  ngAfterViewInit() {
-    const control = this.control;
-    this.formProperty.valueChanges.subscribe((newValue) => {
-      if (this.isEquivalent(this.controlValue, newValue)) {
-        // Both are equal in value, but could be different strings,
-        // in which case retain user typed string.
-        control.setValue(this.controlValue, { emitEvent: false });
-      } else {
-        // Set control to form property value.
-        control.setValue(newValue, { emitEvent: false });
-      }
-    });
-    control.valueChanges.subscribe((newValue) => {
-      // Override NumberProperty.setValue();
-      this.setPropertyValue(this.formProperty, newValue, false);
-    });
-  }
-
-  /**
-   * This is intended to override this.formProperty.setValue().
-   * The FormPropertyFactory creates the type of FormProperty
-   * based on the field type (number, string, boolean etc) defined in the json schema.
-   *
-   * @param prop - This is NumberProperty type, whose setValue() is bypassed by this call.
-   * @param value - New value to set
-   * @param onlySelf - Flag to update only this component i.e not ancestors, defaults to false.
-   */
-  setPropertyValue(prop: FormProperty, value: any, onlySelf = false) {
-    const re = '^\\s*([\\+\\-]?\\d*\\.?\\d*)';
-    const matches = String(value).match(new RegExp(re));
-    value = matches ? matches[1] : null;
-    this.controlValue = value; // Store user's original string
-    if (typeof value === 'string') {
-      if (value.trim().length === 0) {
-        value = null;
-      } else {
-        const numVal = parseFloat(value);
-        value = isNaN(numVal) ? null : numVal;
-      }
-    }
-    prop._value = value;
-    prop.updateValueAndValidity(onlySelf, true);
-  }
-
-  /**
-   * Compare control's value to what is converted to property value.
-   *
-   * @param controlValue - String value from control
-   * @param propValue - Property value after form property is set.
-   * @return -
-   *   Return true to set controlValue to the control,
-   *   false to set it with property value.
-   */
-  isEquivalent(controlValue: any, propValue: any) {
-    // Accept string starting with decimal or number, even though they are not equal to parseFloat().
-    return controlValue === '.' || controlValue === '-' || parseFloat(controlValue) === parseFloat(propValue);
-  }
+export class NumberComponent extends LfbControlWidgetComponent {
 }
