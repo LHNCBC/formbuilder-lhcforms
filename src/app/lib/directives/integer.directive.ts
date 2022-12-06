475fd7220c3ed02a8f5059c83300041e711237f3
Kanduru, Ajay (NIH/NLM/LHC) [C] <akanduru@mail.nih.gov>
2022-12-06 18:31:05 -0500

    Address review comments

--- src/app/lib/directives/integer.directive.ts
+++ src/app/lib/directives/integer.directive.ts
@@ -1,19 +1,59 @@
 import {Directive, ElementRef, HostListener} from '@angular/core';
 import {LiveAnnouncer} from '@angular/cdk/a11y';
-declare var LForms: any;
 
+/**
+ * A directive to restrict input to integer characters.
+ * It is intended to be used on <input type="number"> element.
+ */
 @Directive({
   selector: '[lfbInteger]'
 })
 export class IntegerDirective {
 
-  constructor(private el: ElementRef, public liveAnnouncer: LiveAnnouncer) {
+  /**
+   * Constructor
+   * @param hostEl - Host element
+   * @param liveAnnouncer - Angular Live Announcer service
+   */
+  constructor(private hostEl: ElementRef, public liveAnnouncer: LiveAnnouncer) {
   }
-  @HostListener('keydown', ['$event'])
-  onKeyDown(event: KeyboardEvent) {
-    if (event.key === '.' || event.key.toLowerCase() === 'e' || (event.key === '-' && this.el.nativeElement.value.startsWith('-')) ) {
-      this.liveAnnouncer.announce(event.key + ' is not accepted for integer input');
+
+  /**
+   * Check input validity.
+   * @param event - Keyboard event object
+   */
+  @HostListener('keyup', ['$event'])
+  OnInput(event: KeyboardEvent) {
+    const el = (event.target as HTMLInputElement);
+    if (! el.validity?.valid) {
+      this.liveAnnouncer.announce('The value is invalid');
       event.preventDefault();
+    }
+  }
+
+  /**
+   * Restrict to integer characters in keydown event.
+   * @param event - Keyboard event object.
+   */
+  @HostListener('keydown', ['$event'])
+  onKeydown(event: KeyboardEvent) {
+    let announce = false;
+    if (
+      event.key === '.' ||
+      event.key.toLowerCase() === 'e' ||
+      (event.key === '-' && (event.target as HTMLInputElement).value.startsWith('-'))) {
+      announce = true;
+      // Key is valid input for number type, but not for integer
+      event.preventDefault();
+    }
+    else if (
+      event.key.length === 1 &&
+      event.code.startsWith('Key')
+    ) {
+      announce = true;
+    }
+    if (announce) {
+      this.liveAnnouncer.announce(event.key + ' is not accepted for integer');
     }
   }
 
@@ -23,7 +63,7 @@ export class IntegerDirective {
     const parsedVal = String(parseInt(val, 10));
     if (val !== parsedVal) {
       if (parsedVal !== 'NaN') {
-        this.el.nativeElement.value += parsedVal;
+        this.hostEl.nativeElement.value += parsedVal;
       }
       event.preventDefault();
     }
