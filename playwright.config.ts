38dffcc6a6cb67ab33d59a1f3066ce6bd22e856d
Woody Amornkul <woody.amornkul@nih.gov>
2025-02-24 10:27:31 -0500

    Removed the timeout for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,6 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
 
-  timeout: 120_000,
 
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
