ed67f30579a43f340abcb50f79f0f7be8815d352
Woody Amornkul <woody.amornkul@nih.gov>
2025-04-28 21:26:14 -0400

    Fixed the Cypress failed test.

--- cypress/e2e/item-level/item-level.cy.ts
+++ cypress/e2e/item-level/item-level.cy.ts
@@ -3,6 +3,7 @@
 import {Util} from '../../../src/app/lib/util';
 import {CypressUtil} from '../../support/cypress-util';
 import {ExtensionDefs} from "../../../src/app/lib/extension-defs";
+//import 'cypress-real-events';
 
 const entryFormatUrl = 'http://hl7.org/fhir/StructureDefinition/entryFormat';
 const olpExtUrl = 'http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-observationLinkPeriod';
@@ -3847,11 +3848,11 @@ describe('Home page', () => {
         cy.get('#variable-label-2').clear().type('c_fhir_query_obs');
         cy.get('#variable-type-2').select('FHIR Query (Observation)');
         //cy.get('input#simple-expression-2').type('12');
-        cy.get('lhc-query-observation').shadow().find('#autocomplete-2').type('weight{downarrow}{enter}');
+        cy.get('lhc-query-observation').shadow().find('#autocomplete-2').type('weight', { delay: 100 }).type('{downarrow}{enter}');
         //cy.get('#searchResults #completionOptions tr').contains('29463-7').click();
         cy.get('div#row-2 lhc-query-observation').shadow().within(() => {
           cy.get('div.query-select > span.autocomp_selected > ul > li')
-            .should('have.text', '×weight'); 
+            .should('have.text', '×Weight - 29463-7'); 
         });
 
         // Add a new variable 'd_question'
@@ -3861,7 +3862,8 @@ describe('Home page', () => {
         cy.get('#question-3')
           .should('exist')
           .should('be.visible')
-          .type('Pick Initial Value (Single){downarrow}{enter}');
+          .type('Pick Initial Value (Single)', { delay: 100 })
+          .type('{downarrow}{enter}');
         //cy.get('span#completionOptions').contains('29463-7').click();
 
         // Add a new variable 'e_easy_path_exp'
@@ -3892,7 +3894,8 @@ describe('Home page', () => {
 
       cy.get('@thirdVariable').find('td:nth-child(1)').should('have.text', 'c_fhir_query_obs');
       cy.get('@thirdVariable').find('td:nth-child(2)').should('have.text', 'FHIR Query (Observation)');
-      cy.get('@thirdVariable').find('td:nth-child(3)').should('have.text', "Observation?code=weight&date=gt{{today()-1 months}}&patient={{%patient.id}}&_sort=-date&_count=1");
+      //cy.get('@thirdVariable').find('td:nth-child(3)').should('have.text', "Observation?code=weight&date=gt{{today()-1 months}}&patient={{%patient.id}}&_sort=-date&_count=1");
+      cy.get('@thirdVariable').find('td:nth-child(3)').should('have.text', "Observation?code=http%3A%2F%2Floinc.org%7C29463-7&date=gt{{today()-1 months}}&patient={{%patient.id}}&_sort=-date&_count=1");
 
       cy.get('@fourthVariable').find('td:nth-child(1)').should('have.text', 'd_question');
       cy.get('@fourthVariable').find('td:nth-child(2)').should('have.text', 'Question');
