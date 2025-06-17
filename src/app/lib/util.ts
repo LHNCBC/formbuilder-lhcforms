bc105fb6c35f0d2c7f280b551fdec65a374d2e05
Woody Amornkul <woody.amornkul@nih.gov>
2025-06-17 09:25:33 -0400

    Removed commented out function

--- src/app/lib/util.ts
+++ src/app/lib/util.ts
@@ -713,22 +713,6 @@ export class Util {
   }
 
   /**
-   * Determines whether the provided array of answer options contains data. The array may
-   * contain an empty object; therefore, it is necessary to validate the 'display' and
-   * 'code' fields. 
-   * @param ansOpts - An array of Answer Options objects
-   * @returns - true if the Answer Option array is empty or contain one empty item, otherwise false.
-   */
-/*   static isEmptyAnswerOption(ansOpts: any): boolean {
-    if (!ansOpts || ansOpts.length === 0) {
-      return true;
-    }
-
-    return !ansOpts.some(ansOpt => (ansOpt?.valueCoding?.display && ansOpt?.valueCoding?.code));
-  }
- */
-
-  /**
    * Determines whether a valueCoding object is considered empty.
    * A valueCoding is considered empty if either its 'display' or 'code' property is missing or falsy.
    *
