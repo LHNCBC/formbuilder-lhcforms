eacb4cf26a2f102a3ff745dfaef4ca1ecc4dd0be
Woody Amornkul <woody.amornkul@nih.gov>
2026-01-03 11:16:55 -0500

    Updated playwright test.

--- tests/enable-when.spec.ts
+++ tests/enable-when.spec.ts
@@ -4,12 +4,13 @@ import {PWUtils} from "./pw-utils";
 
 test.describe('enableWhen condition and enableWhenExpression', async () => {
   let mainPO: MainPO;
+  let fileJson;
 
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
     mainPO = new MainPO(page);
     await mainPO.loadILPage();
-    await PWUtils.uploadFile(page, './fixtures/enable-when-expression-sample.json', true);
+    fileJson = await PWUtils.uploadFile(page, './fixtures/enable-when-expression-sample.json', true);
     await page.getByRole('button', { name: 'Edit questions' }).last().click();
   });
 
@@ -20,7 +21,7 @@ test.describe('enableWhen condition and enableWhenExpression', async () => {
     await PWUtils.expandAdvancedFields(page);
 
     // Validate that the enableWhen expression radio button is checked.
-    const enableWhenExpressionButton = await PWUtils.getRadioButton(page, 'Conditional method', 'enableWhenExpression');
+    const enableWhenExpressionButton = await PWUtils.getRadioButton(page, 'Conditional method', 'enableWhen expression');
     await enableWhenExpressionButton.check();
 
     // Check the enableWhenExpression extension.
@@ -28,43 +29,7 @@ test.describe('enableWhen condition and enableWhenExpression', async () => {
     await expect(input).toHaveValue('%a > 5 and %b > 5');
     let q = await PWUtils.getQuestionnaireJSONWithoutUI(page, 'R5');
     expect(q.item[3].extension).toHaveLength(3);
-    expect(q.item[3].extension).toEqual([
-      {
-        "url": "http://hl7.org/fhir/StructureDefinition/variable",
-        "valueExpression": {
-          "name": "a",
-          "language": "text/fhirpath",
-          "expression": "%resource.item.where(linkId='717943342309').answer.value",
-          "extension": [
-            {
-              "url": "http://lhcforms.nlm.nih.gov/fhirExt/expression-editor-variable-type",
-              "valueString": "question"
-            }
-          ]
-        }
-      },
-      {
-        "url": "http://hl7.org/fhir/StructureDefinition/variable",
-        "valueExpression": {
-          "name": "b",
-          "language": "text/fhirpath",
-          "expression": "%resource.item.where(linkId='261918238396').answer.value",
-          "extension": [
-            {
-              "url": "http://lhcforms.nlm.nih.gov/fhirExt/expression-editor-variable-type",
-              "valueString": "question"
-            }
-          ]
-        }
-      },
-      {
-        "url": "http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-enableWhenExpression",
-        "valueExpression": {
-          "language": "text/fhirpath",
-          "expression": "%a > 5 and %b > 5"
-        }
-      }
-    ]);
+    expect(q.item[3].extension).toEqual(fileJson.item[3].extension);
 
     // Select the 'enableWhen condition and behavior' option.
     const enableWhenConditionLabel = await PWUtils.getRadioButtonLabel(page, 'Conditional method', 'enableWhen condition and behavior');
@@ -73,80 +38,17 @@ test.describe('enableWhen condition and enableWhenExpression', async () => {
     // The enableWhenExpression extension should be hidden.
     q = await PWUtils.getQuestionnaireJSONWithoutUI(page, 'R5');
     expect(q.item[3].extension).toHaveLength(2);
-    expect(q.item[3].extension).toEqual([
-      {
-        "url": "http://hl7.org/fhir/StructureDefinition/variable",
-        "valueExpression": {
-          "name": "a",
-          "language": "text/fhirpath",
-          "expression": "%resource.item.where(linkId='717943342309').answer.value",
-          "extension": [
-            {
-              "url": "http://lhcforms.nlm.nih.gov/fhirExt/expression-editor-variable-type",
-              "valueString": "question"
-            }
-          ]
-        }
-      },
-      {
-        "url": "http://hl7.org/fhir/StructureDefinition/variable",
-        "valueExpression": {
-          "name": "b",
-          "language": "text/fhirpath",
-          "expression": "%resource.item.where(linkId='261918238396').answer.value",
-          "extension": [
-            {
-              "url": "http://lhcforms.nlm.nih.gov/fhirExt/expression-editor-variable-type",
-              "valueString": "question"
-            }
-          ]
-        }
-      }
-    ]);
+
+    expect(q.item[3].extension[0]).toEqual(fileJson.item[3].extension[0]);
+    expect(q.item[3].extension[1]).toEqual(fileJson.item[3].extension[1]);
 
     // Select the 'enableWhenExpression' option.
-    const enableWhenExpressionLabel = await PWUtils.getRadioButtonLabel(page, 'Conditional method', 'enableWhenExpression');
+    const enableWhenExpressionLabel = await PWUtils.getRadioButtonLabel(page, 'Conditional method', 'enableWhen expression');
     await enableWhenExpressionLabel.click();
 
     // The enableWhenExpression extension should be visible again.
     q = await PWUtils.getQuestionnaireJSONWithoutUI(page, 'R5');
     expect(q.item[3].extension).toHaveLength(3);
-    expect(q.item[3].extension).toEqual([
-      {
-        "url": "http://hl7.org/fhir/StructureDefinition/variable",
-        "valueExpression": {
-          "name": "a",
-          "language": "text/fhirpath",
-          "expression": "%resource.item.where(linkId='717943342309').answer.value",
-          "extension": [
-            {
-              "url": "http://lhcforms.nlm.nih.gov/fhirExt/expression-editor-variable-type",
-              "valueString": "question"
-            }
-          ]
-        }
-      },
-      {
-        "url": "http://hl7.org/fhir/StructureDefinition/variable",
-        "valueExpression": {
-          "name": "b",
-          "language": "text/fhirpath",
-          "expression": "%resource.item.where(linkId='261918238396').answer.value",
-          "extension": [
-            {
-              "url": "http://lhcforms.nlm.nih.gov/fhirExt/expression-editor-variable-type",
-              "valueString": "question"
-            }
-          ]
-        }
-      },
-      {
-        "url": "http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-enableWhenExpression",
-        "valueExpression": {
-          "language": "text/fhirpath",
-          "expression": "%a > 5 and %b > 5"
-        }
-      }
-    ]);
+    expect(q.item[3].extension).toEqual(fileJson.item[3].extension);
   });
 });
